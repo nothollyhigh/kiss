3411d6f48e123fefc817231ddf9c55b0772e6445
nothollyhigh <47098491+nothollyhigh@users.noreply.github.com>
2019-09-26 20:37:21 +0800

    - optimize error

--- util/error.go
+++ util/error.go
@@ -1,14 +1,41 @@
 package util
 
 import (
+	"errors"
 	"fmt"
 	"runtime"
 )
 
+var (
+	MAX_ERROR_STACK   = 128
+	ERROR_LINE_PREFIX = "\n\t"
+)
+
 type Error struct {
-	text  string
-	stack string
-	child interface{}
+	Deep  int
+	Text  string
+	Stack string
+	Child *Error
+}
+
+func (e *Error) ToError() error {
+	return errors.New(e.Text)
+}
+
+func (e *Error) Root() *Error {
+	if e == nil {
+		return nil
+	}
+
+	for i := 0; i < MAX_ERROR_STACK; i++ {
+		if e.Child == nil {
+			return e
+		}
+		e = e.Child
+	}
+
+	panic(fmt.Errorf("error stack overflow, MAX_ERROR_STACK: %v", MAX_ERROR_STACK))
+	return nil
 }
 
 func (e *Error) Error() string {
@@ -17,24 +44,13 @@ func (e *Error) Error() string {
 	}
 
 	var (
-		ok  = true
 		err = e
-		idx = 1
-		str = "\n"
+		str = ""
 	)
 
-	for ok {
-		str += fmt.Sprintf("\tstack: %d %v %v\n", idx, err.stack, err.text)
-		if err.child == nil {
-			break
-		}
-		idx++
-		tmp, ok := err.child.(*Error)
-		if !ok {
-			str += fmt.Sprintf("\tstack: %d %v %v\n", idx, err.stack, err.child)
-			break
-		}
-		err = tmp
+	for err != nil {
+		str += fmt.Sprintf("%v%v %v", ERROR_LINE_PREFIX, err.Stack, err.Text)
+		err = err.Child
 	}
 
 	return str
@@ -46,20 +62,43 @@ func Errorf(format string, v ...interface{}) *Error {
 	)
 
 	return &Error{
-		text:  fmt.Sprintf(format, v...),
-		child: nil,
-		stack: fmt.Sprintf("[file: %s] [func: %s] [line: %d]", file, runtime.FuncForPC(pc).Name(), line),
+		Deep:  1,
+		Text:  fmt.Sprintf(format, v...),
+		Child: nil,
+		Stack: fmt.Sprintf("[file: %s] [func: %s] [line: %d]", file, runtime.FuncForPC(pc).Name(), line),
 	}
 }
 
 func ErrorWrap(text string, v interface{}) *Error {
 	var (
 		pc, file, line, _ = runtime.Caller(1)
+
+		ret = &Error{
+			Text:  text,
+			Stack: fmt.Sprintf("[file: %s] [func: %s] [line: %d]", file, runtime.FuncForPC(pc).Name(), line),
+		}
 	)
 
-	return &Error{
-		text:  text,
-		child: v,
-		stack: fmt.Sprintf("[file: %s] [func: %s] [line: %d]", file, runtime.FuncForPC(pc).Name(), line),
+	e, ok := v.(*Error)
+	if ok {
+		ret.Child = e
+	} else {
+		ret.Child = Errorf("%v", v)
 	}
+
+	ret.Deep = e.Deep + 1
+
+	if ret.Deep > MAX_ERROR_STACK {
+		panic(fmt.Errorf("error stack overflow, MAX_ERROR_STACK: %v", MAX_ERROR_STACK))
+	}
+
+	return ret
+}
+
+func SetMaxErrorStack(max int) {
+	MAX_ERROR_STACK = max
+}
+
+func SetErrorLinePrefix(prefix string) {
+	ERROR_LINE_PREFIX = prefix
 }
