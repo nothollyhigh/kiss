5fb0ad1625e3bbb47eacf6050f262250becf873d
nothollyhigh <47098491+nothollyhigh@users.noreply.github.com>
2019-09-26 20:40:18 +0800

    - revert

--- net/wsengine.go
+++ net/wsengine.go
@@ -4,248 +4,246 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/nothollyhigh/kiss/log"
 	"github.com/nothollyhigh/kiss/util"
-	"net/http"
-	"os"
-	"sync/atomic"
+	"sync"
 	"time"
 )
 
-type WSServer struct {
-	*WSEngine
+// websocket engine
+type WSEngine struct {
+	sync.Mutex
+	sync.WaitGroup
 
-	// http server
-	*HttpServer
+	// 序列化
+	Codec ICodec
 
-	// websocket upgrader
-	upgrader *websocket.Upgrader
+	// 读超时时间
+	ReadTimeout time.Duration
 
-	// http请求过滤
-	requestHandler func(w http.ResponseWriter, r *http.Request) error
+	// 写超时时间
+	WriteTimeout time.Duration
 
-	// ws连接成功
-	connectHandler func(cli *WSClient, w http.ResponseWriter, r *http.Request) error
+	// 读最大包长限制
+	ReadLimit int64
 
-	// 连接断开
-	disconnectHandler func(cli *WSClient, w http.ResponseWriter, r *http.Request)
+	// 发送队列容量
+	SendQSize int
 
-	// 当前连接数
-	currLoad int64
+	// websocket消息类型
+	MessageType int
 
-	// 过载保护,同时最大连接数
-	maxLoad int64
+	// shutdown flag
+	shutdown bool
 
-	// handlers map[uint32]func(cli *WSClient, cmd uint32, data []byte)
+	//ctypto cipher
+	cipher ICipher
 
-	// all clients
-	clients map[*WSClient]struct{}
+	// message handlers
+	handlers map[uint32]func(cli *WSClient, msg IMessage)
 
-	// http handler
-	httpHandler func(w http.ResponseWriter, r *http.Request)
+	// user defined message handler
+	messageHandler func(cli *WSClient, msg IMessage)
 
-	// routers
-	wsRoutes map[string]func(http.ResponseWriter, *http.Request)
+	// receive message handler
+	recvHandler func(cli *WSClient) IMessage
+
+	// send message handler
+	sendHandler func(cli *WSClient, data []byte) error
+
+	// send queue full handler
+	sendQueueFullHandler func(cli *WSClient, msg interface{})
+
+	// new cipher handler
+	newCipherHandler func() ICipher
 }
 
-// serve http
-func (s *WSServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if h, ok := s.wsRoutes[r.URL.Path]; ok {
-		h(w, r)
-		return
+// receive message
+func (engine *WSEngine) RecvMsg(cli *WSClient) IMessage {
+	if engine.recvHandler != nil {
+		return engine.recvHandler(cli)
 	}
 
-	if s.httpHandler != nil {
-		s.httpHandler(w, r)
-		return
+	var err error
+	var data []byte
+
+	if cli.ReadTimeout > 0 {
+		err = cli.Conn.SetReadDeadline(time.Now().Add(cli.ReadTimeout))
+		if err != nil {
+			log.Debug("Websocket SetReadDeadline failed: %v", err)
+			return nil
+		}
 	}
 
-	http.NotFound(w, r)
+	_, data, err = cli.Conn.ReadMessage()
+	if err != nil {
+		log.Debug("Websocket ReadMessage failed: %v", err)
+		return nil
+	}
+
+	msg := &Message{
+		rawData: data,
+		data:    nil,
+	}
+
+	if _, err = msg.Decrypt(cli.RecvSeq(), cli.RecvKey(), cli.Cipher()); err != nil {
+		log.Debug("%s RecvMsg Decrypt Err: %v", cli.Conn.RemoteAddr().String(), err)
+		return nil
+	}
+
+	return msg
 }
 
-// setting websocket upgrader
-func (s *WSServer) SetUpgrader(upgrader *websocket.Upgrader) {
-	s.upgrader = upgrader
-}
-
-// handle websocket request
-func (s *WSServer) onWebsocketRequest(w http.ResponseWriter, r *http.Request) {
+// send websocket data
+func (engine *WSEngine) Send(cli *WSClient, data []byte) error {
 	defer util.HandlePanic()
 
-	if s.shutdown {
-		http.NotFound(w, r)
-		return
+	if engine.sendHandler != nil {
+		return engine.sendHandler(cli, data)
 	}
 
-	if s.requestHandler != nil && s.requestHandler(w, r) != nil {
-		http.NotFound(w, r)
-		return
-	}
-
-	online := atomic.AddInt64(&s.currLoad, 1)
-
-	if s.maxLoad > 0 && online > s.maxLoad {
-		atomic.AddInt64(&s.currLoad, -1)
-		http.NotFound(w, r)
-		return
-	}
-
-	conn, err := s.upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		http.NotFound(w, r)
-		return
-	}
-
-	var cli = newClient(conn, s.WSEngine)
-	s.Lock()
-	s.clients[cli] = struct{}{}
-	s.Unlock()
-
-	conn.SetReadLimit(s.ReadLimit)
-
-	defer func() {
-		s.Lock()
-		delete(s.clients, cli)
-		s.Unlock()
-		atomic.AddInt64(&s.currLoad, -1)
-
-		cli.Stop()
-
-		if s.disconnectHandler != nil {
-			s.disconnectHandler(cli, w, r)
-		}
-	}()
-
-	if s.connectHandler != nil && s.connectHandler(cli, w, r) != nil {
-		return
-	}
-
-	conn.SetPingHandler(func(string) error {
-		if s.ReadTimeout > 0 {
-			conn.SetReadDeadline(time.Now().Add(s.ReadTimeout))
-		}
-		return nil
-	})
-	conn.SetPongHandler(func(string) error {
-		if s.ReadTimeout > 0 {
-			conn.SetReadDeadline(time.Now().Add(s.ReadTimeout))
-		}
-		return nil
-	})
-
-	go cli.writeloop()
-
-	cli.readloop()
-}
-
-// current load
-func (server *WSServer) CurrLoad() int64 {
-	return atomic.LoadInt64(&server.currLoad)
-}
-
-// max load
-func (server *WSServer) MaxLoad() int64 {
-	return server.maxLoad
-}
-
-// setting max concurrent
-func (server *WSServer) SetMaxConcurrent(maxLoad int64) {
-	server.maxLoad = maxLoad
-}
-
-// client num
-func (s *WSServer) ClientNum() int {
-	s.Lock()
-	defer s.Unlock()
-	return len(s.clients)
-}
-
-// setting request handler
-func (s *WSServer) HandleRequest(h func(w http.ResponseWriter, r *http.Request) error) {
-	s.requestHandler = h
-}
-
-// setting websocket connection handler
-func (s *WSServer) HandleConnect(h func(cli *WSClient, w http.ResponseWriter, r *http.Request) error) {
-	s.connectHandler = h
-}
-
-// setting websocket disconnected handler
-func (s *WSServer) HandleDisconnect(h func(cli *WSClient, w http.ResponseWriter, r *http.Request)) {
-	s.disconnectHandler = h
-}
-
-// setting websocket router
-func (s *WSServer) HandleWs(path string) {
-	s.wsRoutes[path] = s.onWebsocketRequest
-}
-
-// setting http router
-func (s *WSServer) HandleHttp(h func(w http.ResponseWriter, r *http.Request)) {
-	s.httpHandler = h
-}
-
-// stop all websocket clients
-func (s *WSServer) stopClients() {
-	s.Lock()
-	defer s.Unlock()
-
-	for client, _ := range s.clients {
-		client.Stop()
-	}
-}
-
-// graceful shutdown
-func (s *WSServer) Shutdown(timeout time.Duration, cb func(error)) {
-	s.Lock()
-	shutdown := s.shutdown
-	s.shutdown = true
-	s.Unlock()
-	if !shutdown {
-		log.Debug("WSServer Shutdown ...")
-
-		if timeout <= 0 {
-			timeout = DefaultShutdownTimeout
-		}
-
-		done := make(chan struct{}, 1)
-		util.Go(func() {
-			s.Wait()
-
-			s.stopClients()
-
-			s.HttpServer.Shutdown()
-
-			done <- struct{}{}
-		})
-
-		after := time.NewTimer(timeout)
-		defer after.Stop()
-		select {
-		case <-after.C:
-			log.Debug("WSServer Shutdown timeout")
-			if cb != nil {
-				cb(ErrWSEngineShutdownTimeout)
-			}
-		case <-done:
-			log.Debug("WSServer Shutdown success")
-			cb(nil)
-		}
-	}
-}
-
-// websocket server factory
-func NewWebsocketServer(tag string, addr string) (*WSServer, error) {
 	var err error
-	svr := &WSServer{
-		WSEngine: NewWebsocketEngine(),
-		maxLoad:  DefaultMaxOnline,
-		upgrader: &websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }},
-		clients:  map[*WSClient]struct{}{},
-		wsRoutes: map[string]func(http.ResponseWriter, *http.Request){},
+
+	if cli.WriteTimeout > 0 {
+		err = cli.Conn.SetWriteDeadline(time.Now().Add(engine.WriteTimeout))
+		if err != nil {
+			log.Debug("%s Send SetReadDeadline Err: %v", cli.Conn.RemoteAddr().String(), err)
+			cli.Stop()
+			return err
+		}
 	}
 
-	svr.HttpServer, err = NewHttpServer(tag, addr, svr, time.Second*5, nil, func() {
-		os.Exit(-1)
+	err = cli.Conn.WriteMessage(engine.MessageType, data)
+	if err != nil {
+		log.Debug("%s Send Write Err: %v", cli.Conn.RemoteAddr().String(), err)
+		cli.Stop()
+	}
+
+	return err
+}
+
+// setting user defined message handler
+func (engine *WSEngine) HandleMessage(h func(cli *WSClient, msg IMessage)) {
+	engine.messageHandler = h
+}
+
+// setting message handler by cmd
+func (engine *WSEngine) Handle(cmd uint32, h func(cli *WSClient, msg IMessage)) {
+	if cmd == CmdPing {
+		log.Panic(ErrorReservedCmdPing.Error())
+	}
+	if cmd == CmdSetReaIp {
+		log.Panic(ErrorReservedCmdSetRealip.Error())
+	}
+	if cmd == CmdRpcMethod {
+		log.Panic(ErrorReservedCmdRpcMethod.Error())
+	}
+	if cmd == CmdRpcError {
+		log.Panic(ErrorReservedCmdRpcError.Error())
+	}
+	if cmd > CmdUserMax {
+		log.Panic(ErrorReservedCmdInternal.Error())
+	}
+	if _, ok := engine.handlers[cmd]; ok {
+		log.Panic("Websocket Handle failed, cmd %v already exist", cmd)
+	}
+	engine.handlers[cmd] = h
+}
+
+// setting receive message handler
+func (engine *WSEngine) HandleRecv(recver func(cli *WSClient) IMessage) {
+	engine.recvHandler = recver
+}
+
+// setting send message handler
+func (engine *WSEngine) HandleSend(sender func(cli *WSClient, data []byte) error) {
+	engine.sendHandler = sender
+}
+
+// handle send queue full
+func (engine *WSEngine) OnSendQueueFull(cli *WSClient, msg interface{}) {
+	if engine.sendQueueFullHandler != nil {
+		engine.sendQueueFullHandler(cli, msg)
+	}
+}
+
+// setting send queue full handler
+func (engine *WSEngine) HandleSendQueueFull(h func(cli *WSClient, msg interface{})) {
+	engine.sendQueueFullHandler = h
+}
+
+// new cipher
+func (engine *WSEngine) NewCipher() ICipher {
+	if engine.newCipherHandler != nil {
+		return engine.newCipherHandler()
+	}
+	return nil
+}
+
+// setting new cipher handler
+func (engine *WSEngine) HandleNewCipher(newCipher func() ICipher) {
+	engine.newCipherHandler = newCipher
+}
+
+// handle message
+func (engine *WSEngine) onMessage(cli *WSClient, msg IMessage) {
+	if engine.shutdown {
+		// switch msg.Cmd() {
+		// case CmdPing:
+		// case CmdSetReaIp:
+		// case CmdRpcMethod:
+		// case CmdRpcError:
+		// default:
+		// 	log.Debug("engine is not running, ignore cmd %X, ip: %v", msg.Cmd(), cli.Ip())
+		// 	return
+		// }
+		return
+	}
+
+	if engine.messageHandler != nil {
+		engine.messageHandler(cli, msg)
+		return
+	}
+
+	cmd := msg.Cmd()
+	if cmd == CmdPing {
+		cli.SendMsg(NewMessage(CmdPing2, nil))
+		return
+	}
+	if cmd == CmdPing2 {
+		return
+	}
+
+	if h, ok := engine.handlers[cmd]; ok {
+		engine.Add(1)
+		defer engine.Done()
+		defer util.HandlePanic()
+		h(cli, msg)
+	} else {
+		log.Debug("Websocket no handler for cmd: %v", cmd)
+	}
+}
+
+// websocket engine factory
+func NewWebsocketEngine() *WSEngine {
+	engine := &WSEngine{
+		Codec:        DefaultCodec,
+		ReadTimeout:  DefaultReadTimeout,
+		WriteTimeout: DefaultWriteTimeout,
+		ReadLimit:    DefaultReadLimit,
+		SendQSize:    DefaultSendQSize,
+		MessageType:  websocket.TextMessage,
+		shutdown:     false,
+		handlers: map[uint32]func(*WSClient, IMessage){
+			CmdSetReaIp: func(cli *WSClient, msg IMessage) {
+				ip := msg.Body()
+				cli.SetRealIp(string(ip))
+			},
+		},
+	}
+
+	cipher := NewCipherGzip(DefaultThreshold)
+	engine.HandleNewCipher(func() ICipher {
+		return cipher
 	})
 
-	return svr, err
+	return engine
 }
