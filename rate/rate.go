0b933322cdd4a4158a6d0d06da96d1e591e2d1e9
nothollyhigh <47098491+nothollyhigh@users.noreply.github.com>
2019-10-12 22:21:41 +0800

    - deprecated

--- rate/rate.go
+++ rate/rate.go
@@ -1,88 +1,105 @@
 package rate
 
-import (
-	"fmt"
-	"github.com/nothollyhigh/kiss/log"
-	"github.com/nothollyhigh/kiss/timer"
-	"runtime"
-	"sync"
-	"time"
-)
+// deprecated
 
-var (
-	defaultLimiter = &Limiter{
-		trigger:    timer.New("kiss-default-limiter"),
-		bucketsMap: map[string]chan empty{},
-	}
-)
+// import (
+// 	"fmt"
+// 	"github.com/nothollyhigh/kiss/log"
+// 	"github.com/nothollyhigh/kiss/timer"
+// 	"runtime"
+// 	"sync"
+// 	"time"
+// )
 
-type empty struct{}
+// var (
+// 	defaultLimiter = &Limiter{
+// 		trigger:    timer.New("kiss-default-limiter"),
+// 		bucketsMap: map[string]chan empty{},
+// 	}
 
-type Limiter struct {
-	sync.RWMutex
-	trigger    *timer.Timer
-	bucketsMap map[string]chan empty
-}
+// 	ErrTimeout = fmt.Errorf("rate Wait timeout")
+// )
 
-func (l *Limiter) get(times int, interval time.Duration, constant bool, tags ...interface{}) chan empty {
-	_, file, line, _ := runtime.Caller(2)
-	key := fmt.Sprintf("%s_%d", file, line)
-	for _, v := range tags {
-		key += fmt.Sprintf("_%v", v)
-	}
+// type empty struct{}
 
-	l.Lock()
-	defer l.Unlock()
+// type Limiter struct {
+// 	sync.RWMutex
+// 	trigger    *timer.Timer
+// 	bucketsMap map[string]chan empty
+// }
 
-	buckets, ok := l.bucketsMap[key]
-	if ok {
-		return buckets
-	}
+// func (l *Limiter) get(times int, interval time.Duration, constant bool, tags ...interface{}) chan empty {
+// 	_, file, line, _ := runtime.Caller(2)
+// 	key := fmt.Sprintf("%s_%d", file, line)
+// 	for _, v := range tags {
+// 		key += fmt.Sprintf("_%v", v)
+// 	}
 
-	if constant {
-		buckets = make(chan empty, 1)
-		interval = interval / time.Duration(times)
-		l.trigger.Schedule(0, interval, 0, func() {
-			select {
-			case buckets <- empty{}:
-			default:
-			}
+// 	l.Lock()
+// 	defer l.Unlock()
 
-		})
-	} else {
-		buckets = make(chan empty, times)
-		l.trigger.Schedule(0, interval, 0, func() {
-			for i := 0; i < times; i++ {
-				select {
-				case buckets <- empty{}:
-				default:
-				}
-			}
-		})
-	}
+// 	buckets, ok := l.bucketsMap[key]
+// 	if ok {
+// 		return buckets
+// 	}
 
-	l.bucketsMap[key] = buckets
+// 	if constant {
+// 		buckets = make(chan empty, 1)
+// 		interval = interval / time.Duration(times)
+// 		l.trigger.Schedule(0, interval, 0, func() {
+// 			select {
+// 			case buckets <- empty{}:
+// 			default:
+// 			}
 
-	return buckets
-}
+// 		})
+// 	} else {
+// 		buckets = make(chan empty, times)
+// 		l.trigger.Schedule(0, interval, 0, func() {
+// 			for i := 0; i < times; i++ {
+// 				select {
+// 				case buckets <- empty{}:
+// 				default:
+// 				}
+// 			}
+// 		})
+// 	}
 
-func (l *Limiter) Wait(times int, interval time.Duration, constant bool, tags ...interface{}) {
-	if times < 1 {
-		log.Panic("rate.Wait failed: [invalid times arg: %v]", times)
-	}
-	if interval < 1 {
-		log.Panic("rate.Wait failed: [invalid interval arg: %v]", interval)
-	}
-	<-l.get(times, interval, constant, tags...)
-}
+// 	l.bucketsMap[key] = buckets
 
-func New(tag string) *Limiter {
-	return &Limiter{
-		trigger:    timer.New(fmt.Sprintf("kiss-limiter-%v", tag)),
-		bucketsMap: map[string]chan empty{},
-	}
-}
+// 	return buckets
+// }
 
-func Wait(times int, interval time.Duration, constant bool, tags ...interface{}) {
-	defaultLimiter.Wait(times, interval, constant, tags)
-}
+// func (l *Limiter) Wait(times int, interval time.Duration, constant bool, timeout time.Duration, tags ...interface{}) error {
+// 	if times < 1 {
+// 		log.Panic("rate Wait failed: [invalid times arg: %v]", times)
+// 	}
+// 	if interval < 1 {
+// 		log.Panic("rate Wait failed: [invalid interval arg: %v]", interval)
+// 	}
+
+// 	if timeout > 0 {
+// 		after := time.NewTimer(timeout)
+// 		defer after.Stop()
+// 		select {
+// 		case <-l.get(times, interval, constant, tags...):
+// 		case <-after.C:
+// 			return ErrTimeout
+// 		}
+// 	} else {
+// 		<-l.get(times, interval, constant, tags...)
+// 	}
+
+// 	return nil
+// }
+
+// func New(tag string) *Limiter {
+// 	return &Limiter{
+// 		trigger:    timer.New(fmt.Sprintf("kiss-limiter-%v", tag)),
+// 		bucketsMap: map[string]chan empty{},
+// 	}
+// }
+
+// func Wait(times int, interval time.Duration, constant bool, timeout time.Duration, tags ...interface{}) error {
+// 	return defaultLimiter.Wait(times, interval, constant, timeout, tags)
+// }
